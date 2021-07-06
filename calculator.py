7377d71f1ca9bab44d621bcd60e3f247956b4c32
marvin-karis <marvinirungu58@gmail.com>
2021-07-06 19:02:21 +0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,7 @@
+sum = 0
+dig = int(input("type number"))
+for num in range(1,dig):
+    sum = sum +num
+print("sum of digit is:",sum)
+avarage = sum / len(range(1,dig))
+print("shared ration:",avarage) 
