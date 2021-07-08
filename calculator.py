a279b838d3afb0efc3806ea1272bf960cf8f1a3f
marvin-karis <marvinirungu58@gmail.com>
2021-07-08 18:15:49 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,4 +4,5 @@ for num in range(1,dig):
     sum = sum +num
 print("sum of digit is:",sum)
 avarage = sum / len(range(1,dig))
-print("shared ration:",avarage) 
+print("shared ration:",avarage)
+#initial
